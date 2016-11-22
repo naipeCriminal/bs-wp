371aba2e43e99bc8abf342e86610163d9903cbf9
elvis gabriel martinez lopez <elvisgabrielmartinezlopez@MacBook-Pro-de-elvis.local>
2016-11-22 11:01:09 -0600

    plantilla base

--- themes/bs-wp/header.php
+++ themes/bs-wp/header.php
@@ -9,91 +9,227 @@
  * @since Twenty Sixteen 1.0
  */
 
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js">
+?>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<?php endif; ?>
+	<meta charset="UTF-8">
+	<title>Document</title>
 	<?php wp_head(); ?>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	<link rel="stylesheet" href="<?php echo bloginfo('template_url'); ?>/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo bloginfo('template_url'); ?>/css/custom.css">
 </head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 nopadding">
 
-<body <?php body_class(); ?>>
-<div id="page" class="site">
-	<div class="site-inner">
-		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentysixteen' ); ?></a>
+				 <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+				  <div class="panel panel-default">
+				    <div class="panel-heading" role="tab" id="headingOne">
+				      <h4 class="panel-title">
+				        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+				          Quienes Somos
+				        </a>
+				      </h4>
+				    </div>
+				    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+				      <div class="panel-body">
+				                         <ul id="SubMenu" class="SubMenus">
+				                   <li><a href="quien_preve.asp" target="contenedorSitio">¿Qué es Seguros Salud Bancomer?</a></li>
+				                   <li><a href="quien_negoc.asp" target="contenedorSitio">Línea de Negocio</a></li>
+				                   <li><a href="normatividad.asp" target="contenedorSitio">Información Financiera</a></li>
+				                   <li><a href="quien_cfd.asp" target="contenedorSitio">Comprobantes Fiscales Digitales</a></li>
+				                       </ul>        
 
-		<header id="masthead" class="site-header" role="banner">
-			<div class="site-header-main">
-				<div class="site-branding">
-					<?php twentysixteen_the_custom_logo(); ?>
+				      </div>
+				    </div>
+				  </div>
+				  <div class="panel panel-default">
+				    <div class="panel-heading" role="tab" id="headingTwo">
+				      <h4 class="panel-title">
+				        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+				          Nuestros Productos
+				        </a>
+				      </h4>
+				    </div>
+				    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+				      <div class="panel-body">
+				                  <ul id="SubMenu" class="SubMenus">
+				                    <li><a href="quien_negoc_segga.asp" target="contenedorSitio">Seguro de GMM</a></li>
+				                    <li><a href="quien_negoc_segsa.asp" target="contenedorSitio">Seguro de Salud</a></li>
+				                    <li><a href="quien_negoc_smb.asp" target="contenedorSitio">Seguro Médico Bancomer</a></li>
+				                    <li><a href="quien_negoc_sbe.asp" target="contenedorSitio">Seguros Básicos Estandarizados</a></li>
+				                    </ul>
 
-					<?php if ( is_front_page() && is_home() ) : ?>
-						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php else : ?>
-						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php endif;
+				      </div>
+				    </div>
+				  </div>
+				  <div class="panel panel-default">
+				    <div class="panel-heading" role="tab" id="headingThree">
+				      <h4 class="panel-title">
+				        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+				          Medicina Preventiva
+				        </a>
+				      </h4>
+				    </div>
+				    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+				      <div class="panel-body">
+				----
+				      </div>
+				    </div>
+				  </div>
+				  <div class="panel panel-default">
+				    <div class="panel-heading" role="tab" id="headingThree">
+				      <h4 class="panel-title">
+				        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+				          Gastos Médicos Mayores
+				        </a>
+				      </h4>
+				    </div>
+				    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+				      <div class="panel-body">
+				             <ul id="SubMenu" class="SubMenus">
+				            <li><a href="gamem_descr.asp" target="contenedorSitio">Descripción</a></li>
+				            <!-- (I) OPimentel - 15082014 - MC140815-02 -->
+				            <li><a href="/DirectorioMedico/index.asp" target="contenedorSitio">Directorio Médico</a></li>
+				            <!-- (I) OPimentel - 15082014 - MC140815-02 -->
+				             <li><a href="gamem_sini.asp" target="contenedorSitio">En caso de siniestro</a></li>
+				             <li><a href="gamem_forre.asp" target="contenedorSitio">Formatos</a></li>
+				             <li><a href="gamem_Contacto.asp" target="contenedorSitio">Contacto</a></li>
+				             <li><a href="gamem_otrser_1.asp" target="contenedorSitio">Otros Servicios</a></li>
+				             <li><a href="gamem_condicionsGral.asp" target="contenedorSitio">Condiciones Generales</a></li>   
+				             </ul>
+				      </div>
+				    </div>
+				  </div>
+				      <div class="panel panel-default">
+				    <div class="panel-heading" role="tab" id="headingThree">
+				      <h4 class="panel-title">
+				        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+				          Salud
+				        </a>
+				      </h4>
+				    </div>
+				    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+				      <div class="panel-body">
+				         <ul id="SubMenu" class="SubMenus">
+				          <li><a href="salud_descri.asp" target="contenedorSitio">Descripción</a></li>
+				          <li><a href="./DirectorioMedico/index.asp" target="contenedorSitio">Directorio Médico</a></li>
+				          <li><a href="salud_atencion_medica.asp" target="contenedorSitio">Atención Médica</a></li>
+				          <!--(I) LHerrera, 1270-Cambio de Nombre y Logo  26/09/2013-->
+				          <li><a href="gastossalud/index.asp?servicio=5" target="contenedorSitio">Atención Dental</a></li>
+				          <!--(F) LHerrera, 1270-Cambio de Nombre y Logo  26/09/2013-->
+				          <li><a href="salud_forre.asp" target="contenedorSitio">Formatos</a></li>
+				          <li><a href="salud_Contacto.asp" target="contenedorSitio">Contacto</a></li>
+				          <li><a href="salud_otrser_1.asp" target="contenedorSitio">Otros Servicios</a></li>
+				          <li><a href="pdf/Condiciones Generales Salud colectiva-2.pdf" target="contenedorSitio">Condiciones Generales</a></li>
+				        <li><a href="lista_medicamentos.asp" target="contenedorSitio">Lista de Medicamentos</a></li>
+				          </ul>
 
-					$description = get_bloginfo( 'description', 'display' );
-					if ( $description || is_customize_preview() ) : ?>
-						<p class="site-description"><?php echo $description; ?></p>
-					<?php endif; ?>
-				</div><!-- .site-branding -->
 
-				<?php if ( has_nav_menu( 'primary' ) || has_nav_menu( 'social' ) ) : ?>
-					<button id="menu-toggle" class="menu-toggle"><?php _e( 'Menu', 'twentysixteen' ); ?></button>
+				      </div>
+				    </div>
+				  </div>
+				  <div class="panel panel-default">
+				    <div class="panel-heading" role="tab" id="headingThree">
+				      <h4 class="panel-title">
+				        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+				          Seguro Médico Bancomer
+				        </a>
+				      </h4>
+				    </div>
+				    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+				      <div class="panel-body">
+				                 <ul id="SubMenu" class="SubMenus">
+				                      <li><a href="segumed_descri.asp" target="contenedorSitio">Descripción</a></li>
+				                      <li><a href="segumed_planes.asp" target="contenedorSitio">Planes y coberturas</a></li>
+				                      <li><a href="segumed_directorio.asp" target="contenedorSitio">Directorio Médico</a></li>
+				                      <li><a href="segumed_que_hacer.asp" target="contenedorSitio">Qué hacer en caso de</a></li>
+				                      <li><a href="segumed_contacto.asp" target="contenedorSitio">Contacto</a></li>
+				                <li><a href="segumed_condicionsGral.asp" target="contenedorSitio">Condiciones Generales</a></li>          
+				                     </ul> 
+				      </div>
+				    </div>
+				  </div>
+				  <div class="panel panel-default">
+				    <div class="panel-heading" role="tab" id="headingThree">
+				      <h4 class="panel-title">
+				        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+				          Respaldo CirugíaSegura BBVA Bancomer
+				        </a>
+				      </h4>
+				    </div>
+				    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+				      <div class="panel-body">
+				                 <ul id="SubMenu" class="SubMenus">
+				                      <li><a href="cirugiaseg_descri.asp" target="contenedorSitio">Descripción</a></li>
+				                      <li><a href="cirugiaseg_planes.asp" target="contenedorSitio">Planes y coberturas</a></li>
+				                      <li><a href="cirugiaseg_directorio.asp" target="contenedorSitio">Directorio Médico</a></li>
+				                      <li><a href="cirugiaseg_que_hacer.asp" target="contenedorSitio">En caso de siniestro</a></li>
+				                      <li><a href="cirugiaseg_forre.asp" target="contenedorSitio">Formatos</a></li>
+				                      <li><a href="cirugiaseg_contacto.asp" target="contenedorSitio">Contacto</a></li>
+				                 <li><a href="pdf/Condiciones_Generales_Respaldo_CirugiaSegura-1.pdf" target="contenedorSitio">Condiciones Generales</a></li>   
+				                     </ul> 
+				      </div>
+				    </div>
+				  </div>
+				  <div class="panel panel-default">
+				    <div class="panel-heading" role="tab" id="headingThree">
+				      <h4 class="panel-title">
+				        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+				          Seguro de Gastos Médicos Individual Básico Estandarizado
+				        </a>
+				      </h4>
+				    </div>
+				    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+				      <div class="panel-body">
+				                 <ul id="SubMenu_GM" class="SubMenus">
+				                     <li><a href="gmmb_descri.asp" target="contenedorSitio">Descripción</a></li>
+				                     <li><a href="gmmb_pregfrec.asp" target="contenedorSitio">Preguntas Frecuentes</a></li>
+				                     <li><a href="pdf/CG_Gastos_Medicos.pdf" target="contenedorSitio">Condiciones Generales</a></li>
+				                   </ul>
+				      </div>
+				    </div>
+				  </div>
+				  <div class="panel panel-default">
+				    <div class="panel-heading" role="tab" id="headingThree">
+				      <h4 class="panel-title">
+				        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+				          Seguro de Salud Dental Estandarizado
+				        </a>
+				      </h4>
+				    </div>
+				    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+				      <div class="panel-body">
+				                 <ul id="SubMenu" class="SubMenus">
+				                       <li><a href="admb_descri.asp" target="contenedorSitio">Descripción</a></li>
+				                       <li><a href="pdf/CG_Dental.pdf" target="contenedorSitio">Condiciones Generales</a></li>
+				                     </ul>  
+				      </div>
+				    </div>
+				  </div>
+				  <div class="panel panel-default">
+				    <div class="panel-heading" role="tab" id="headingThree">
+				      <h4 class="panel-title">
+				        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+				          Seguro Individual o Familiar de Salud Básico Estandarizado
+				        </a>
+				      </h4>
+				    </div>
+				    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+				      <div class="panel-body">
+				                 <ul id="SubMenu" class="SubMenus">
+				                       <li><a href="ammb_descri.asp" target="contenedorSitio">Descripción</a></li>
+				                       <li><a href="pdf/CG_Salud.pdf" target="contenedorSitio">Condiciones Generales</a></li>
+				                     </ul> 
+				      </div>
+				    </div>
+				  </div>                
+				</div>
 
-					<div id="site-header-menu" class="site-header-menu">
-						<?php if ( has_nav_menu( 'primary' ) ) : ?>
-							<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Primary Menu', 'twentysixteen' ); ?>">
-								<?php
-									wp_nav_menu( array(
-										'theme_location' => 'primary',
-										'menu_class'     => 'primary-menu',
-									 ) );
-								?>
-							</nav><!-- .main-navigation -->
-						<?php endif; ?>
+				
+			</div>
+			<div class="col-md-9 nopadding">
 
-						<?php if ( has_nav_menu( 'social' ) ) : ?>
-							<nav id="social-navigation" class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Social Links Menu', 'twentysixteen' ); ?>">
-								<?php
-									wp_nav_menu( array(
-										'theme_location' => 'social',
-										'menu_class'     => 'social-links-menu',
-										'depth'          => 1,
-										'link_before'    => '<span class="screen-reader-text">',
-										'link_after'     => '</span>',
-									) );
-								?>
-							</nav><!-- .social-navigation -->
-						<?php endif; ?>
-					</div><!-- .site-header-menu -->
-				<?php endif; ?>
-			</div><!-- .site-header-main -->
-
-			<?php if ( get_header_image() ) : ?>
-				<?php
-					/**
-					 * Filter the default twentysixteen custom header sizes attribute.
-					 *
-					 * @since Twenty Sixteen 1.0
-					 *
-					 * @param string $custom_header_sizes sizes attribute
-					 * for Custom Header. Default '(max-width: 709px) 85vw,
-					 * (max-width: 909px) 81vw, (max-width: 1362px) 88vw, 1200px'.
-					 */
-					$custom_header_sizes = apply_filters( 'twentysixteen_custom_header_sizes', '(max-width: 709px) 85vw, (max-width: 909px) 81vw, (max-width: 1362px) 88vw, 1200px' );
-				?>
-				<div class="header-image">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-						<img src="<?php header_image(); ?>" srcset="<?php echo esc_attr( wp_get_attachment_image_srcset( get_custom_header()->attachment_id ) ); ?>" sizes="<?php echo esc_attr( $custom_header_sizes ); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
-					</a>
-				</div><!-- .header-image -->
-			<?php endif; // End header image check. ?>
-		</header><!-- .site-header -->
-
-		<div id="content" class="site-content">
--- themes/bs-wp/page.php
+++ themes/bs-wp/page.php
@@ -13,8 +13,6 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
 		<?php
 		// Start the loop.
 		while ( have_posts() ) : the_post();
@@ -31,11 +29,4 @@ get_header(); ?>
 		endwhile;
 		?>
 
-	</main><!-- .site-main -->
-
-	<?php get_sidebar( 'content-bottom' ); ?>
-
-</div><!-- .content-area -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
